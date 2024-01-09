be3c5365fcdbbac4a33cd58e33a40b675572eeee
Martín (Netux) Rodriguez <send.email@netux.site>
2024-01-09 08:25:13 -0300

    refactor: create persistent game object for the plugin, move patches to their own class

---
+++ Patches.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace OpenDoorsInSpace.Patches
+{
+	[HarmonyPatch]
+	public class Patches
+	{
+        [HarmonyPatch(typeof(HangarShipDoor), nameof(HangarShipDoor.Update))]
+        [HarmonyPostfix]
+        public static void HanagerShipDoor_Update_Postfix(HangarShipDoor __instance)
+        {
+            if (!__instance.triggerScript.interactable && StartOfRound.Instance.inShipPhase)
+            {
+                __instance.triggerScript.interactable = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(HangarShipDoor), nameof(HangarShipDoor.PlayDoorAnimation))]
+        [HarmonyPrefix]
+        public static bool HangarShipDoor_PlayDoorAnimation_Prefix(bool closed)
+        {
+            Plugin.Log.LogDebug($"HangarShipDoor.PlayDoorAnimation(closed: {closed}) Prefix");
+            if (!closed && StartOfRound.Instance.inShipPhase)
+            {
+                Plugin.Manager.EjectDueToNegligance();
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.EndPlayersFiredSequenceClientRpc))]
+        [HarmonyPrefix]
+        public static void StartOfRound_EndPlayersFiredSequenceClientRpc_Prefix()
+        {
+            Plugin.Log.LogDebug($"StartOfRound.EndPlayersFiredSequenceClientRpc() Prefix");
+            Plugin.Manager.ResetEjectingDueToNegligance();
+        }
+
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.playersFiredGameOver), MethodType.Enumerator)]
+        [HarmonyPrefix]
+        public static void StartOfRound_playersFiredGameOver_Enumerator_Prefix(IEnumerator<object> __instance)
+        {
+            var stateField = AccessTools.DeclaredField(__instance.GetType(), "<>1__state");
+            var currentField = AccessTools.DeclaredField(__instance.GetType(), "<>2__current");
+
+            Plugin.Log.LogDebug($"stateField: {stateField.GetValue(__instance)}");
+            Plugin.Log.LogDebug($"currentField: {currentField.GetValue(__instance)}");
+
+            if (Plugin.Manager != null && Plugin.Manager.IsEjectingDueToNegligence && ((int)stateField.GetValue(__instance)) < 2)
+            {
+                // Skip 5s wait, alarm and fired speech sfx, and another ~9s wait before opening the doors
+                stateField.SetValue(__instance, 2);
+            }
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,53 +1,34 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using System.Collections.Generic;
 using System.Reflection;
 using TMPro;
-using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace OpenDoorsInSpace
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        internal static Plugin Instance;
         internal static ManualLogSource Log;
         internal static OpenDoorsInSpacePluginManager Manager;
 
         public Plugin()
         {
-            Instance = this;
             Log = base.Logger;
-
-            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }
 
         private void Awake()
         {
-            SceneManager.sceneLoaded += HandleSceneLoaded;
+            var gameObject = new GameObject("OpenDoorsInSpace");
+            gameObject.AddComponent<OpenDoorsInSpacePluginManager>();
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+            DontDestroyOnLoad(gameObject);
+            Manager = gameObject.GetComponent<OpenDoorsInSpacePluginManager>();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
-
-        private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
-        {
-            if (scene.name == "SampleSceneRelay" && mode == LoadSceneMode.Single)
-            {
-                if (Manager?.gameObject == null)
-                {
-                    var gameObject = new GameObject("OpenDoorsInSpace", [typeof(OpenDoorsInSpacePluginManager)]);
-                    Instantiate(
-                        gameObject,
-                        gameObject.transform.position,
-                        gameObject.transform.rotation,
-                        scene.GetRootGameObjects()[0].transform
-                    );
-                    Manager = gameObject.GetComponent<OpenDoorsInSpacePluginManager>();
-                }
-            }
         }
     }
 
@@ -55,13 +36,10 @@ namespace OpenDoorsInSpace
     {
         public bool IsEjectingDueToNegligence { get; internal set; }
 
-        private TextMeshProUGUI youAreFiredDueTopNegliganceSubtextTMP;
-
+        private TextMeshProUGUI youAreFiredDueToNegliganceSubtextTMP;
         void Start()
         {
             Plugin.Log.LogDebug("OpenDoorsInSpacePluginManager Start");
-
-            youAreFiredDueTopNegliganceSubtextTMP = CreateYouAreFiredDueToNegliganceSubtext();
         }
 
         private GameObject GetYouAreFiredOriginalReasonSubtextGameObject()
@@ -76,10 +54,10 @@ namespace OpenDoorsInSpace
             youAreFiredNegliganceReasonSubtextGameObject.name = youAreFiredNegliganceReasonSubtextGameObject.name + " (OpenDoorsInSpace fired subtext)";
             youAreFiredNegliganceReasonSubtextGameObject.SetActive(false);
 
-            youAreFiredDueTopNegliganceSubtextTMP = youAreFiredNegliganceReasonSubtextGameObject.GetComponent<TextMeshProUGUI>();
-            youAreFiredDueTopNegliganceSubtextTMP.text = "Someone opened the hangar door while in space.";
+            youAreFiredDueToNegliganceSubtextTMP = youAreFiredNegliganceReasonSubtextGameObject.GetComponent<TextMeshProUGUI>();
+            youAreFiredDueToNegliganceSubtextTMP.text = "Someone opened the hangar door while in space.";
 
-            return youAreFiredDueTopNegliganceSubtextTMP;
+            return youAreFiredDueToNegliganceSubtextTMP;
         }
 
         public void EjectDueToNegligance()
@@ -93,12 +71,12 @@ namespace OpenDoorsInSpace
 
             StartOfRound.Instance.ManuallyEjectPlayersServerRpc();
 
-            if (youAreFiredDueTopNegliganceSubtextTMP == null)
+            if (youAreFiredDueToNegliganceSubtextTMP == null)
             {
-                youAreFiredDueTopNegliganceSubtextTMP = CreateYouAreFiredDueToNegliganceSubtext();
+                youAreFiredDueToNegliganceSubtextTMP = CreateYouAreFiredDueToNegliganceSubtext();
             }
             this.GetYouAreFiredOriginalReasonSubtextGameObject()?.SetActive(false);
-            youAreFiredDueTopNegliganceSubtextTMP.gameObject.SetActive(true);
+            youAreFiredDueToNegliganceSubtextTMP.gameObject.SetActive(true);
 
         }
 
@@ -109,61 +87,6 @@ namespace OpenDoorsInSpace
             //youAreFiredNegliganceReasonSubtextTMP?.gameObject.SetActive(false);
 
             IsEjectingDueToNegligence = false;
-        }
-    }
-
-    [HarmonyPatch(typeof(HangarShipDoor), nameof(HangarShipDoor.Update))]
-    public class HangarDoorUpdatePatch
-    {
-        public static void Postfix(HangarShipDoor __instance)
-        {
-            if (!__instance.triggerScript.interactable && StartOfRound.Instance.inShipPhase)
-            {
-                __instance.triggerScript.interactable = true;
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(HangarShipDoor), nameof(HangarShipDoor.PlayDoorAnimation))]
-    public class HangarDoorPlayDoorAnimationPatch
-    {
-        public static bool Prefix(bool closed)
-        {
-            Plugin.Log.LogDebug($"HangarShipDoor.PlayDoorAnimation(closed: {closed}) Prefix");
-            if (!closed && StartOfRound.Instance.inShipPhase)
-            {
-                Plugin.Manager.EjectDueToNegligance();
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.EndPlayersFiredSequenceClientRpc))]
-    public class StartOfRoundEndPlayersFiredSequenceClientRpcPatch
-    {
-        public static void Prefix()
-        {
-            Plugin.Log.LogDebug($"StartOfRound.EndPlayersFiredSequenceClientRpc() Prefix");
-            Plugin.Manager.ResetEjectingDueToNegligance();
-        }
-    }
-
-    [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.playersFiredGameOver), MethodType.Enumerator)]
-    public class StartOfRoundPlayersFiredGameOverEnumeratorPatch
-    {
-        public static void Prefix(IEnumerator<object> __instance)
-        {
-            var stateField = AccessTools.DeclaredField(__instance.GetType(), "<>1__state");
-            var currentField = AccessTools.DeclaredField(__instance.GetType(), "<>2__current");
-
-            Plugin.Log.LogDebug($"stateField: {stateField.GetValue(__instance)}");
-            Plugin.Log.LogDebug($"currentField: {currentField.GetValue(__instance)}");
-
-            if (Plugin.Manager != null && Plugin.Manager.IsEjectingDueToNegligence && ((int) stateField.GetValue(__instance)) < 2)
-            {
-                // Skip 5s wait, alarm and fired speech sfx, and another ~9s wait before opening the doors
-                stateField.SetValue(__instance, 2);
-            }
         }
     }
 }
